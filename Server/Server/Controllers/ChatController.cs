ac05b938267dcc8000f76a35060da5e011d9f3ed
AlexanderMikhailenko <molotok539@gmail.com>
2022-06-10 19:52:33 +1000

    Add NumOfUnreadMessages Http requeset

--- Server/Server/Controllers/ChatController.cs
+++ Server/Server/Controllers/ChatController.cs
@@ -12,11 +12,13 @@ namespace Server.Controllers
 		}
 
         public IActionResult Index() {
-			return View(context.Messages.AsEnumerable().DistinctBy(m => m.ChatId));
+			var uniqueChats = context.Messages.AsEnumerable().DistinctBy(m => m.ChatId).ToList();
+			return View(uniqueChats.Select(c => new Tuple<Message, int>(c, NumOfUnreadMessages(c.ChatId, Message.SourceType.Player))));
 		}
 
 		public IActionResult Chat(Guid chatId) {
-			return View(new Tuple<Guid, IEnumerable<Message>>(chatId, context.Messages.AsEnumerable().Where(m => m.ChatId == chatId)));
+			var messages = context.Messages.Where(m => m.ChatId == chatId);
+			return base.View(new Tuple<Guid, IEnumerable<Message>>(chatId, messages));
 		}
 
 		public IActionResult PlayerChat() {
@@ -25,6 +27,15 @@ namespace Server.Controllers
 
 		public IActionResult ChatHistory(Guid chatId) {
 			return Json(context.Messages.AsEnumerable().Where(m => m.ChatId == chatId).ToArray());
+		}
+
+		public ActionResult<int> NumOfUnreadOperatorMessages(Guid chatId) {
+			return NumOfUnreadMessages(chatId, Message.SourceType.Operator);
+		}
+
+		private int NumOfUnreadMessages(Guid chatId, Message.SourceType sourceType) {
+			var messages = context.Messages.Where(m => m.ChatId == chatId);
+			return messages.Count(m => m.Type == sourceType && m.IsReaded == false);
 		}
 	}
 }
