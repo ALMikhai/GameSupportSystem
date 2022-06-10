52d5a6fb1d262d0d1c8c01840009a58d8420eb70
AlexanderMikhailenko <molotok539@gmail.com>
2022-06-10 19:51:20 +1000

    Add MarkAsRead functionality to ChatHub

--- Server/Server/Hubs/ChatHub.cs
+++ Server/Server/Hubs/ChatHub.cs
@@ -21,6 +21,7 @@ namespace Server.Hubs
 				await SendError(playerTokenId, "This token is not valid, please re-login.");
 				return;
 			}
+			await MarkOperatorMessagesAsRead(playerTokenId);
 			var token = await context.Tokens.FirstAsync(t => t.Id == playerTokenId);
 			var player = await context.Players.FirstAsync(p => p.DeviceId == token.PlayerDeviceId);
 			var nickname = player.Nickname;
@@ -40,6 +41,7 @@ namespace Server.Hubs
 				logger.LogError($"Token is not valid {playerTokenId}");
 				return;
 			}
+			await MarkPlayerMessagesAsRead(playerTokenId);
 			var name = "Operator";
 			var messageObject = new Message() {
 				ChatId = playerTokenId,
@@ -52,12 +54,30 @@ namespace Server.Hubs
 			await ReceiveMessage(playerTokenId, messageObject);
 		}
 
+		public async Task MarkPlayerMessagesAsRead(Guid chatId) {
+			await MarkMessagesAsRead(chatId, Message.SourceType.Player);
+		}
+
+		public async Task MarkOperatorMessagesAsRead(Guid chatId) {
+			await MarkMessagesAsRead(chatId, Message.SourceType.Operator);
+		}
+
 		private async Task ReceiveMessage(Guid playerTokenId, Message message) {
 			await Clients.All.SendAsync($"ReceiveMessage-{playerTokenId}", JsonConvert.SerializeObject(message));
 		}
 
 		private async Task SendError(Guid playerTokenId, string message) {
 			await Clients.All.SendAsync($"ReceiveError-{playerTokenId}", message);
+		}
+
+		private async Task MarkMessagesAsRead(Guid chatId, Message.SourceType from) {
+			var messages = context.Messages.Where(m => m.ChatId == chatId);
+			foreach (var message in messages) {
+				if (message.Type == from) {
+					message.IsReaded = true;
+				}
+			}
+			await context.SaveChangesAsync();
 		}
 	}
 }
